e9db48948acbf614a0a39e0198af36e045cbd8dd
Daniil <121315611+dev-daniil@users.noreply.github.com>
2023-10-03 16:58:13 +0300

    Update openvpn-install.sh

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -3,12 +3,12 @@ sudo apt install -y openvpn
 sudo apt-get -y install openvpn-auth-radius
 mkdir ~/easy-rsa
 wget https://github.com/OpenVPN/easy-rsa/releases/download/v3.1.2/EasyRSA-3.1.2.tgz | tar xz -C
-echo 'set_var EASYRSA_REQ_COUNTRY    "US"
+echo "set_var EASYRSA_REQ_COUNTRY    "US"
       set_var EASYRSA_REQ_PROVINCE   "California"
       set_var EASYRSA_REQ_CITY       "San Francisco"
       set_var EASYRSA_REQ_ORG        "Copyleft Certificate Co"
       set_var EASYRSA_REQ_EMAIL      "me@example.net"
-      set_var EASYRSA_REQ_OU         "My Organizational Unit"' > ~/easy-rsa-3.0.1/easyrsa3/vars
+      set_var EASYRSA_REQ_OU         "My Organizational Unit"" > ~/easy-rsa-3.0.1/easyrsa3/vars
 cd ~/easy-rsa-3.0.1/easyrsa3 && rm -rf ./pki && ./easyrsa --batch init-pki && ./easyrsa --batch build-ca nopass && ./easyrsa --batch --days=3650 build-server-full server nopass && ./easyrsa --batch --days=3650 build-client-full client nopass && ./easyrsa --days=3650 --batch gen-crl
 rm -rf /etc/openvpn/server
 mkdir /etc/openvpn/server
@@ -20,7 +20,7 @@ cp ~/easy-rsa-3.0.1/easyrsa3/pki/private/ca.key /etc/openvpn/server/ca.key
 cp ~/easy-rsa-3.0.1/easyrsa3/pki/private/server.key /etc/openvpn/server/server.key
 cp ~/easy-rsa-3.0.1/easyrsa3/pki/private/client.key /etc/openvpn/server/client.key
 rm ~/EasyRSA-3.1.2.tgz
-echo '
+echo "
             NAS-Identifier=OpenVpn
 
             Service-Type=5
@@ -45,17 +45,17 @@ echo '
             wait=1
             sharedsecret=SERVER_RADIUS_PASSWORD
     }
-    ' > /etc/openvpn/radiusplugin.cnf
+    " > /etc/openvpn/radiusplugin.cnf
 openvpn --genkey --secret /etc/openvpn/server/tc.key
-echo '-----BEGIN DH PARAMETERS-----
+echo "-----BEGIN DH PARAMETERS-----
 MIIBCAKCAQEA//////////+t+FRYortKmq/cViAnPTzx2LnFg84tNpWp4TZBFGQz
 +8yTnc4kmz75fS/jY2MMddj2gbICrsRhetPfHtXV/WVhJDP1H18GbtCFY2VVPe0a
 87VXE15/V8k1mE8McODmi3fipona8+/och3xWKE2rec1MKzKT0g6eXq8CrGCsyT7
 YdEIqUuyyOP7uWrat2DX9GgdT0Kj3jlN9K5W7edjcrsZCwenyO4KbXCeAvzhzffi
 7MA0BM0oNC9hkXL+nOmFg/+OTxIy7vKBg8P+OxtMb61zO7X8vC7CIAXFjvGDfRaD
 ssbzSibBsu/6iGtCOGEoXJf//////////wIBAg==
------END DH PARAMETERS-----' > /etc/openvpn/server/dh.pem
-echo 'local SERVER_ADDRESS
+-----END DH PARAMETERS-----" > /etc/openvpn/server/dh.pem
+echo "local SERVER_ADDRESS
 port 1194
 proto udp
 dev tun
@@ -82,7 +82,7 @@ persist-tun
 verb 3
 crl-verify crl.pem
 explicit-exit-notify
-topology subnet' > /etc/openvpn/server/server.conf
+topology subnet" > /etc/openvpn/server/server.conf
 {
 echo "client
 dev tun
@@ -115,3 +115,4 @@ systemctl stop openvpn-server@server
 systemctl disable openvpn-server@server
 systemctl enable openvpn-server@server
 systemctl start openvpn-server@server
+
