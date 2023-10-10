cb8125cd603bcde8c93625b904a1689075d467a9
Daniil <121315611+dev-daniil@users.noreply.github.com>
2023-10-10 17:41:02 +0300

    Update install-ss-libev.sh

--- install-ss-libev.sh
+++ install-ss-libev.sh
@@ -6,7 +6,7 @@ echo '{
     "local_port":1080,
     "password":"SS_PASS",
     "timeout":60,
-    "method":"aes-128-gcm"
+    "method":"aes-256-gcm"
 }' > /etc/shadowsocks-libev/config.json
 
 systemctl restart shadowsocks-libev
